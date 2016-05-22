3ba36f17c41db30301af8ac8116aae7d82dc0c46
Steven Liekens <steven.liekens@gmail.com>
2016-05-23 00:12:22 +0200

    Add UserInformationParser

--- src/Uri/userinfo/UserInformation.cs
+++ src/Uri/userinfo/UserInformation.cs
@@ -8,38 +8,5 @@ namespace Uri.userinfo
             : base(repetition)
         {
         }
-
-        public string Password
-        {
-            get
-            {
-                var indexOfSeparator = Text.IndexOf(':');
-                if (indexOfSeparator == -1)
-                {
-                    return null;
-                }
-
-                return Text.Substring(indexOfSeparator + 1);
-            }
-        }
-
-        public string UserName
-        {
-            get
-            {
-                if (Text == string.Empty)
-                {
-                    return null;
-                }
-
-                var indexOfSeparator = Text.IndexOf(':');
-                if (indexOfSeparator == -1)
-                {
-                    return Text;
-                }
-
-                return Text.Substring(0, indexOfSeparator);
-            }
-        }
     }
 }
---
+++ src/Uri/userinfo/UserInformationParser.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Txt.Core;
+
+namespace Uri.userinfo
+{
+    public class UserInformationParser : Parser<UserInformation, NetworkCredential>
+    {
+        protected override NetworkCredential ParseImpl(UserInformation value)
+        {
+            if (string.IsNullOrEmpty(value.Text))
+            {
+                return new NetworkCredential();
+            }
+            var indexOfSeparator = value.Text.IndexOf(':');
+            if (indexOfSeparator == -1)
+            {
+                return new NetworkCredential { UserName = value.Text };
+            }
+            var userName = value.Text.Substring(0, indexOfSeparator);
+            var password = value.Text.Substring(indexOfSeparator + 1);
+            return new NetworkCredential(userName, password);
+        }
+    }
+}
